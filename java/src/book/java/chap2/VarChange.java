72b3f5d84cba59013feea556aaeb62a21a723a83
bing <bing@DESKTOP-L0S2GO9>
2019-05-11 18:37:55 +0900

    commit

--- java/src/book/java/chap2/VarChange.java
+++ java/src/book/java/chap2/VarChange.java
@@ -25,10 +25,36 @@ public class VarChange {
 		
 		// 작은 크기 타입 = (작은크기타입)큰크기타입 : 강제타입변환
 	}
+	
+	void test3() {
+		int a =3;
+		double b = 6.0;
+		double result = a+b;
+		
+		System.out.println(result);
+	}
+	
+	void test4() {
+		byte a = 60;
+		char b = 55;
+		
+		int re = a+b;	
+		System.out.println(re);
+	}
+	
+	void test5() {
+		byte a =1;
+		byte b = 3;
+//		byte re = a+b;
+//		(byte char short int) + (byte char short int) = int형으로 나옴
+//		ㄴ 하나라도 long이 있으면 long타입으로 변환
+	}
 	public static void main(String[] args) {
 		VarChange vc = new VarChange();
 //		vc.test1();
-		vc.test2();
+//		vc.test2();
+//		vc.test3();
+		vc.test4();
 	}
 
 }
