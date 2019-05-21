5296e126fc3c40514cf6ae46129ccdacb9f85c19
bing <bing@DESKTOP-L0S2GO9>
2019-05-21 23:45:18 +0900

    java

--- java/src/book/java/chap4/Message.java
+++ java/src/book/java/chap4/Message.java
@@ -9,13 +9,13 @@ class Message
 		System.out.println("메세지를 입력하세요");
 		System.out.println("프로그램을 종료하려면 q를 입력하세요");
 
-		Scanner scanner = new Scanner(System.in);	//Scanner ��ü ����
+		Scanner scanner = new Scanner(System.in);	//Scanner 객체 생성
 
 		String inputString;
 
 		do{
 			System.out.print(">");
-			inputString = scanner.nextLine();	//Ű����� �Է��� ���ڿ��� ����
+			inputString = scanner.nextLine();	//키보드로 입력한 문자열을 얻음
 			System.out.println(inputString);
 
 		}
