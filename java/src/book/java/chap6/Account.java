ad41ad5d75973606e8fa9a76d39147aeff59dbca
a <a>
2019-05-24 21:42:32 +0900

    java

---
+++ java/src/book/java/chap6/Account.java
@@ -0,0 +1,18 @@
+package book.java.chap6;
+
+public class Account {
+
+	private int balance;
+	
+	final static int MIN_BALANCE=0;
+	final static int MAX_BALANCE=1000000;	//상수는 초기화를 해주어야 함
+	
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		if (balance>=MIN_BALANCE && balance<=MAX_BALANCE) this.balance = balance;
+	}
+	
+
+}
