cb576115e6c358be5eca0dee11c31e975e822562
bing <bing@DESKTOP-L0S2GO9>
2019-05-27 23:37:53 +0900

    java

---
+++ java/src/book/java/chap8/RemoteControlEx.java
@@ -0,0 +1,13 @@
+package book.java.chap8;
+
+public class RemoteControlEx {
+
+	public static void main(String[] args) {
+		
+		RemoteControl rc;
+		rc = new Television();
+		rc = new Audio();
+
+	}
+
+}
---
+++ java/src/book/java/chap8/Television.java
@@ -0,0 +1,29 @@
+package book.java.chap8;
+
+public class Television implements RemoteControl {
+
+	private int volume;
+	
+	//turnOn() 추상메소드의 실체 메소드
+	public void turnOn() {
+		System.out.println("TV를 켭니다");
+	}
+	 
+	//turnOff()
+	public void turnOff() {
+		System.out.println("TV를 끕니다");
+	}
+	
+	//setVolume()
+	public void setVolume(int volume) {
+		if(volume>RemoteControl.MAX_VOLUME) {
+			this.volume = RemoteControl.MAX_VOLUME;
+		}else if(volume<RemoteControl.MIN_VOLUME) {
+			this.volume=RemoteControl.MIN_VOLUME;
+		}else {
+			this.volume=volume;
+		}
+		System.out.println("현재 TV 볼륨 : "+volume);      
+	}
+
+}
