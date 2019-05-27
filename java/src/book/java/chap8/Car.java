63e1f9e165d71fe0f9c00c8da776e513e78a4de7
bing <bing@DESKTOP-L0S2GO9>
2019-05-28 00:56:12 +0900

    java

--- java/src/book/java/chap8/Car.java
+++ java/src/book/java/chap8/Car.java
@@ -2,17 +2,30 @@ package book.java.chap8;
 
 public class Car {
 
-	
+	/*
 	Tire frontLeft = new HankookTire();
 	Tire frontRight = new HankookTire();
 	Tire backLeft = new HankookTire();
 	Tire backRight = new HankookTire();
+	*/
+	
+	Tire[] tires = {
+			new HankookTire(),
+			new HankookTire(),
+			new HankookTire(),
+			new HankookTire()
+	};
 	
 	void run() {
+		/*
 		frontLeft.roll();
 		frontRight.roll();
 		backLeft.roll();
 		backRight.roll();
+		*/
+		for(Tire tire : tires) {
+			tire.roll();
+		}
 	}
 	
 }
---
+++ java/src/book/java/chap8/CarEx.java
@@ -0,0 +1,15 @@
+package book.java.chap8;
+
+public class CarEx {
+
+	public static void main(String[]args) {
+		Car mycar = new Car();
+		
+		mycar.run();
+		
+		mycar.tires[0]= new KumhoTire();
+		mycar.tires[1] = new KumhoTire();
+		
+		mycar.run();
+	}
+}
