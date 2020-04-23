7716b090c311d8d406c8f370c05deb292dee6582
bing <eulvgi@gmail.com>
2020-04-23 17:58:57 +0900

    star2446 complete

--- javap/src/test/backjoon/onetoten/pratice/Star2446.java
+++ javap/src/test/backjoon/onetoten/pratice/Star2446.java
@@ -9,22 +9,44 @@ public class Star2446 {
 	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		
 		int n = Integer.parseInt(br.readLine());
-		int count = (n * 2) - 1;
-		
-		for(int i=1;i<=count;i++) {
-			for(int j=1;i<=n;i++) {
-				
+
+		for(int i=n;i>0;i--) {
+			
+			if(i==1) continue;
+			
+			for(int j=n;j>0;j--) {
+				if(i<j) {
+					System.out.print(" ");
+				}else {
+					System.out.print("*");
+				}
+			}
+			for(int j=1;j<i;j++) {
+				System.out.print("*");
 			}
 			System.out.println();
-			for(int k=n+1;n<=count;k++) {
-				
-			}
+			
+			
 		}
 		
-	
+		for(int i=1;i<=n;i++) {
+			for(int j=n;j>0;j--) {
+				if(i<j) {
+					System.out.print(" ");
+				}else {
+					System.out.print("*");
+				}
+			}
+			for(int j=2;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			
+			
+		}
+		
 	}
 
 }
